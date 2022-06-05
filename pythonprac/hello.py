6673cf956e8cd125f9df99c92469a9e7795e4ccc
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2022-06-06 07:39:30 +0900

    Studied python by making crawling using BeautifulSoup

--- pythonprac/hello.py
+++ pythonprac/hello.py
@@ -1,8 +1,17 @@
-people = [{'name': 'bob', 'age': 20},
-{'name': 'carry', 'age': 38},
-{'name': 'john', 'age': 7},
-{'name': 'smith', 'age': 17},
-{'name': 'ben', 'age': 27}]
-count = 0
-for person in people:
-    print(person['name'])
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=pnt&date=20210829',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+movies = soup.select('#old_content > table > tbody > tr')
+for movie in movies:
+    title = movie.select_one('td.title > div > a')
+
+    if title is not None:
+        title = title.text
+        rank = movie.select_one('td:nth-child(1) > img')['alt']
+        star = movie.select_one('td.point').text
+        print(rank, title, star)
